51d641da6a42a7b5ae96c8a0e02d7ed67a5cb895
rpc <rpc7863@gmail.com>
2022-07-09 19:37:52 +0530

    update UpdateClass

--- enroll/views.py
+++ enroll/views.py
@@ -1,35 +1,32 @@
-from django.shortcuts import render, HttpResponseRedirect,redirect
+from django.shortcuts import render, HttpResponseRedirect, redirect
 from enroll.forms import StudentRegistration
 from enroll.models import User
-from django.views.generic.base import TemplateView,RedirectView
+from django.views.generic.base import TemplateView, RedirectView
 from django.views import View
 
 
 # Create your views here.
-
-
-
 # this function will add new item and show to the front user
+# By using TemplateView class
 class UserAddAndShow(TemplateView):
     template_name = "enroll/addandshow.html"
-    def get_context_data(self, *args,**kwargs):
-        context =  super().get_context_data(**kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
         fm = StudentRegistration()
         stud = User.objects.all()
         context = {
-            'stu':stud,
-            'form':fm
+            'stu': stud,
+            'form': fm
         }
         return context
-    def post(self,request):
+
+    def post(self, request):
         fm = StudentRegistration(request.POST)
         if fm.is_valid():
             fm.save()
         return redirect('/')
-        
-        
-        
-    
+
 
 # def add_show(request):
 #     stud = User.objects.all()
@@ -45,22 +42,22 @@ class UserAddAndShow(TemplateView):
 #     return render(request, "enroll/addandshow.html", {"form": fm, "stu": stud})
 
 
+# this function will edit or update data
+# By using View class
 class UpdateStudent(View):
-    def get(self,request,id):
+    def get(self, request, id):
         pi = User.objects.get(pk=id)
         fm = StudentRegistration(request.POST, instance=pi)
-        return render(request, {"form": fm})
-    def post(self,request,id):
+        return render(request,"enroll/updatestudent.html", {"form": fm})
+
+    def post(self, request, id):
         pi = User.objects.get(pk=id)
-        fm = StudentRegistration(request,instance=pi)
+        fm = StudentRegistration(request, instance=pi)
         if fm.is_valid():
             fm.save()
-    
-            
-        
+        return render(request, "enroll/updatestudent.html", {"form": fm, "stu": pi})
 
 
-# this function will edit or update data
 # def update_data(request, id):
 #     if request.method == "POST":
 #         pi = User.objects.get(pk=id)
@@ -74,14 +71,15 @@ class UpdateStudent(View):
 
 
 # this function will delete data from table
-
+# By using RedirectView class
 class DeleteData(RedirectView):
     url = '/'
+
     def get_redirect_url(self, *args, **kwargs):
-        usr=User.objects.get(pk=kwargs['id'])
+        usr = User.objects.get(pk=kwargs['id'])
         usr.delete()
         return super().get_redirect_url(*args, **kwargs)
-    
+
 # def delete_data(request, id):
 #     if request.method == "POST":
 #         pi = User.objects.get(pk=id)
